14551251115b9609989012000eb265d2fc1a3f80
Harrison Chen <harrisonxchen@gmail.com>
2014-01-25 21:51:14 -0800

    Add home layout and Users controller

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
+
+	subject { page }
+
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_content('TheCodeArchive') }
+		it { should have_title('TheCodeArchive') }
+		it { should_not have_title(' | Home') }
+		it { should have_link('Home') }
+		it { should have_link('Help') }
+		it { should have_link('Contact') }
+	end
+
+	describe "Help page" do
+		before { visit help_path }
+
+		it { should have_content('Help') }
+		it { should have_title('| Help') }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+
+		it { should have_content('Contact') }
+		it { should have_title('| Contact') }
+	end
 end
